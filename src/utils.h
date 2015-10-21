8030b9047e6d5eaad1e102bffbc35a76bf08af5d
Jools64 <joolsreid@live.co.uk>
2015-10-21 19:20:51 +0100

    Added Utils header
    Forgot to add the utils header on the last commit.

---
+++ src/utils.h
@@ -0,0 +1,26 @@
+#define PI 3.141592
+
+typedef struct Pointf
+{
+	float x, y;
+} Pointf;
+
+typedef struct Pointi
+{
+	int x, y;
+} Pointi;
+
+typedef struct Color
+{
+	byte r, g, b, a;
+} Color;
+
+typedef struct Rectanglei
+{
+	int x, y, width, height;
+} Rectanglei;
+
+typedef struct Rectanglef
+{
+	float x, y, width, height;
+} Rectanglef;
